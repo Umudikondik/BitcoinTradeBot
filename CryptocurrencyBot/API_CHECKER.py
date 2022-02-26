b210011a7f9cc7b590008cbc406eb28d0ab3d98c
Umudikondik <55920303+Umudikondik@users.noreply.github.com>
2022-02-26 18:44:06 +0300

    Add files via upload

---
+++ CryptocurrencyBot/API_CHECKER.py
@@ -0,0 +1,3 @@
+## Connection with your wallet. Don't share anyone your API information.
+API_KEY = 'TzsU6Op81oEMQhdhfhwRdHaMSIyG6uyROkPlFYdv1vAUiXpoNwhHKCxTqHSG7x0e'  ## ENTER YOUR API KEY!
+API_SECRET = 'zP3Kwl5dPtuUPOhZUAQJ9AAZ4ZTHa2MrKs6nydFHXsoRhOcrUYaw87NOQn9997GW'  # ENTER YOUR API SECRET!
